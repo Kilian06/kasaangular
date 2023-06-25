afdd7e4776e56e11a922ce9ecbb02c159443fbb4
Kilian Leroy <kilian.leroy@me.com>
2023-06-25 23:20:25 +0200

    init

---
+++ src/app/banniere/banniere.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banniere',
+  templateUrl: './banniere.component.html',
+  styleUrls: ['./banniere.component.scss']
+})
+export class BanniereComponent {
+  baniereImg:any = "assets/taglinehomeimg.png";
+}
